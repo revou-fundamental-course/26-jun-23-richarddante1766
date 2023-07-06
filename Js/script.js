c1bf0f49f572ba0165ef2e7da81440d52d4d0126
richarddante1766 <dante.engineer@gmail.com>
2023-07-06 19:13:17 +0700

    pembaruan terakhir kali

--- Js/script.js
+++ Js/script.js
@@ -12,3 +12,19 @@ function nameValidation() {
     }
     return false;
   }
+
+  let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("imageslider");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
